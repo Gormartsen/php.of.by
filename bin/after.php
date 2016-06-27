8e80fc51a8d8fdbacda33bb37c27f7ccc2f4f6ea
Gor Martsen <Gor.martsen@gmail.com>
2016-06-27 18:16:55 -0400

    After deploy script

---
+++ bin/after.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @file
+ * This script file is executed on the Zen.ci platform for running tests.
+ *
+ * This essentially just wraps around the run-tests.sh script and uses the
+ * result to post the response back to Zen.ci.
+ */
+
+$home = getenv('DOCROOT');
+$deploy_dir = getenv('ZENCI_DEPLOY_DIR');
+chdir($home);
+
+// Success.
+$data = array(
+  'state' => 'success',
+  'message' => 'deployed properly to ' . getenv('DOMAIN'),
+  'summary' => 'Test summary',
+);
+zenci_put_request($data);
+exit(0);
+
+
+/**
+ * Submit a POST request to Zen.ci updating its current status.
+ *
+ * @param array $data
+ *   An array of data to push to Zen.ci. Should include the following:
+ *   - state: One of "error", "success", or "pending".
+ *   - message: A string summary of the state.
+ *   - summary: Optional. A longer description of the state.
+ */
+function zenci_put_request($data) {
+  $token = getenv('ZENCI_API_TOKEN');
+  $status_url = getenv('ZENCI_STATUS_URL');
+
+  $data = json_encode($data);
+
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, $status_url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); // Note the PUT here.
+
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_HEADER, true);
+
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
+      'Token: ' . $token,
+      'Content-Length: ' . strlen($data)
+  ));
+  curl_exec($ch);
+  curl_close($ch);
+}
